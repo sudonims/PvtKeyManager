1642a91335f80639baf35650edd8c3b4a276ab07
Nimish Shah <nimishshah2000@gmail.com>
2021-06-30 00:43:50 +0530

    add stuff

--- lib/main.dart
+++ lib/main.dart
@@ -5,29 +5,28 @@ import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:crypto_key_manager/helpers/FileEncryptDecrypt.dart';
-import 'package:flutter/material.dart';
 import 'package:file_picker/file_picker.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(PvtKeyManager());
 }
 
-class MyApp extends StatelessWidget {
+class PvtKeyManager extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Private Key Manager',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Private Key Manager'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, String this.title}) : super(key: key);
+  MyHomePage({Key key, this.title}) : super(key: key);
 
   final String title;
 
@@ -36,14 +35,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   Future<String> openFile(BuildContext context) async {
     FilePickerResult res = await FilePicker.platform.pickFiles();
     if (res != null)
@@ -55,75 +46,48 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-            TextButton(
-                onPressed: () async {
-                  print("lol");
-                  Encryptor enc = new Encryptor();
-                  enc.lucky = "12";
-                  enc.secret = "nim";
-                  enc.path = await openFile(context);
-                  enc.encrypt();
-                },
-                child: Text("Click")),
-            TextButton(
-                onPressed: () async {
-                  print("lol");
-                  Decryptor enc = new Decryptor();
-                  enc.lucky = "12";
-                  enc.secret = "nim";
-                  enc.path = await openFile(context);
-                  enc.decrypt();
-                },
-                child: Text("Click")),
-            TextButton(
-                onPressed: () {
-                  PrivateKeys keys = new PrivateKeys();
-                  keys.lastModified = DateTime.now();
-                  // PrivateKey key = ;
-                  keys.keys = [
-                    PrivateKey.fromJSON({
-                      "name": "Binance",
-                      "secrets": ["lol", "lol1"]
-                    })
-                  ];
-                  keys.addKey = PrivateKey.fromJSON({
-                    "name": "Binance",
-                    "secrets": ["lol", "lol1aaaaa"]
-                  });
-                  var a = keys.toJSON();
-
-                  print(a);
-
-                  PrivateKeys keysC = PrivateKeys.fromJSON(a);
-                  keysC.addKey = PrivateKey.fromJSON({
-                    "name": "X",
-                    "secrets": ["lulz"]
-                  });
-                  print(keysC.toJSON());
-                },
-                child: Text("Lol"))
-          ],
+        appBar: AppBar(
+          title: Text(widget.title),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
-    );
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextButton(
+                  onPressed: () async {
+                    print("lol");
+                  },
+                  child: Text("Import")),
+              TextButton(onPressed: () async {}, child: Text("Create New")),
+              TextButton(
+                  onPressed: () {
+                    PrivateKeys keys = new PrivateKeys();
+                    keys.lastModified = DateTime.now();
+                    // PrivateKey key = ;
+                    keys.keys = [
+                      PrivateKey.fromJSON({
+                        "name": "Binance",
+                        "secrets": ["lol", "lol1"]
+                      })
+                    ];
+                    keys.addKey = PrivateKey.fromJSON({
+                      "name": "Binance",
+                      "secrets": ["lol", "lol1aaaaa"]
+                    });
+                    var a = keys.toJSON();
+
+                    print(a);
+
+                    PrivateKeys keysC = PrivateKeys.fromJSON(a);
+                    keysC.addKey = PrivateKey.fromJSON({
+                      "name": "X",
+                      "secrets": ["lulz"]
+                    });
+                    print(keysC.toJSON());
+                  },
+                  child: Text("Lol"))
+            ],
+          ),
+        ));
   }
 }
